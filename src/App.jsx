83be66974c5d9895ea0ce442228af27153489c2f
Umar-Nadeem-W <umarnadeem.w@gmail.com>
2024-10-09 21:30:40 +0500

    Added axios api, returned list of menue items and displayed (mapped) them on the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 
+import { useState } from "react"
 import Search from "./components/Search"
 function App() {
+  //foodData is defined here so that it cna be accessible to multiple components
+  const [foodData, setfoodData] =useState([]);
+  //We will send setFoodData to the search componenet so we can change its value
 return(<div>
-    <Search/>
+    <Search setfoodData={setfoodData} />
+    {foodData.map((item)=> <h1>{item.title}</h1>)}
   </div>)
 }
 
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,30 @@
 import { useEffect, useState } from "react"
+import axios from "axios";
 
-export default function Search(){
-
+export default function Search({setfoodData}){
+    const URL = "https://api.spoonacular.com/recipes/complexSearch";
+    const API_KEY = "d29f727c64f3456a9a4360ea5f9474a9";
     const [query, setQuery] = useState("")
+    //syntax of useeffect below
     useEffect(()=>{
-        function myfun(){console.log("The useeffect function was executed");}
-        myfun();
+        async function fetchFood(){
+            try {
+                  const res=  await axios.get(`${URL}`,{
+                        params:{
+                            query:query,
+                            apiKey:API_KEY,
+                        }
+                    })
+                    const data = res.data;
+                    const itemlist = data.results;
+                    console.log(itemlist);
+                    setfoodData(itemlist);
+
+            }catch(error){
+                console.error("Error in getting food data: ",error)
+            }
+        }
+       fetchFood();
     }, [query])
     return (
 
